ffedd26e916321828d3600e05f7c72a27e439cd6
tmkim <taeminkim13@gmail.com>
2024-12-21 10:04:12 -0500

    initial setup for dank bank

---
+++ DEVLOG.md
@@ -0,0 +1,6 @@
+# Development Log
+
+12/21/2024
+- Initialize project
+- Start building API
+- Set up project settings, database, initial urls
---
+++ api_dank_bank/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ApiDankBankConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'api_dank_bank'
